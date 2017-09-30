1a99e7f10a042d4e72c6a0a780392282b8b8d235
Spencer Eick <spencereick@gmail.com>
2017-09-30 16:43:37 -0400

    Fix errors in SQL queries

--- logs.py
+++ logs.py
@@ -5,46 +5,46 @@ import psycopg2
 db = psycopg2.connect(database="news")
 c = db.cursor()
 
-c.execute(" \
+c.execute("\
 SELECT articles.title, count(*) AS num \
 FROM log, articles \
-WHERE POSITION(articles.slug IN log.path) > 0 \
+WHERE log.path = '/article/' || articles.slug \
 GROUP BY articles.title \
 ORDER BY num DESC \
 LIMIT 3;")
 
 top_articles = c.fetchall()
 
-c.execute(" \
+c.execute("\
 SELECT authors.name, count(*) AS num \
 FROM authors, articles, log \
-WHERE POSITION(articles.slug IN log.path) > 0 \
+WHERE log.path = '/article/' || articles.slug \
 AND authors.id = articles.author \
 GROUP BY authors.name \
 ORDER BY num DESC;")
 
 top_authors = c.fetchall()
 
-c.execute(" \
+c.execute("\
 WITH error_requests AS ( \
 SELECT time::date AS day, count(*) AS num \
 FROM log \
 WHERE status = '404 NOT FOUND' \
 GROUP BY day \
 ORDER BY day ASC \
-), ok_requests AS ( \
+), all_requests AS ( \
 SELECT time::date AS day, count(*) AS num \
 FROM log \
-WHERE status = '200 OK' \
 GROUP BY day \
 ORDER BY day ASC \
 ), error_rate AS ( \
-SELECT ok_requests.day, error_requests.num / ok_requests.num::float AS rate \
-FROM error_requests, ok_requests \
-WHERE error_requests.day = ok_requests.day \
-AND ok_requests.num != 0 \
+SELECT all_requests.day, error_requests.num / all_requests.num::float AS rate \
+FROM error_requests, all_requests \
+WHERE error_requests.day = all_requests.day \
+AND all_requests.num != 0 \
 ) \
-SELECT error_rate.day, ROUND(error_rate.rate::numeric * 100, 1) || '%' AS rate\
+SELECT error_rate.day, \
+                    ROUND(error_rate.rate::numeric * 100, 2) || '%' AS rate \
 FROM error_rate \
 WHERE rate > .01;")
 
